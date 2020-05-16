9b0166d1e1a34109b68da5ac3756c0424d078ff8
Brian McClain <brianmmcclain@gmail.com>
2020-05-16 00:41:40 -0400

    Basic IRC and Twitch bot functions

--- src/main/java/com/github/brianmmcclain/reactivebotframework/ReactiveBotFrameworkApplication.java
+++ src/main/java/com/github/brianmmcclain/reactivebotframework/ReactiveBotFrameworkApplication.java
@@ -8,6 +8,14 @@ public class ReactiveBotFrameworkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveBotFrameworkApplication.class, args);
-	}
 
+		String oauth = System.getenv("TWITCH_OAUTH");
+		String nick = System.getenv("TWITCH_NICK");
+		String channel = System.getenv("TWITCH_CHANNEL");
+
+		TwitchBot bot = new TwitchBot();
+		bot.authorize(oauth, nick);
+		bot.joinChannel(channel);
+		bot.sendMessage("Hello!!");
+	}
 }
---
+++ src/main/java/com/github/brianmmcclain/reactivebotframework/TwitchBot.java
@@ -0,0 +1,78 @@
+package com.github.brianmmcclain.reactivebotframework;
+
+import reactor.core.scheduler.Schedulers;
+
+public class TwitchBot {
+    
+    private IRCConnection connection;
+    private boolean isAuthenticated = false;
+    private String channel;
+
+    public TwitchBot() {
+        this.connection = new IRCConnection("irc.chat.twitch.tv", 6667);
+        this.connection.connect();
+    }
+
+    public TwitchBot(String host, int port) {
+        this.connection = new IRCConnection(host, port);
+        this.connection.connect();
+    }
+
+    public boolean isConnected() {
+        return this.connection.isConnected();
+    }
+
+    public boolean isAuthenticated() { 
+        return this.isAuthenticated;
+    }
+
+    public void authorize(String oauth, String nick) {
+        this.connection.send("PASS " + oauth);
+        this.connection.send("NICK " + nick);
+        this.connection.getInputStream()
+        .subscribeOn(Schedulers.parallel())
+        .subscribe( message -> {
+            processMessage(message);
+        });
+    }
+
+    public void joinChannel(String channel) {
+        this.connection.send("JOIN #" + channel);
+        this.channel = channel;
+    }
+
+    public void sendMessage(String message) {
+        System.out.println("SENDING: \"" + message + "\"");
+        this.connection.send("PRIVMSG #" + this.channel + " :" + message);
+    }
+
+    //TODO: This sucks, there's probably a better way to do this
+    public void waitForAuthentication(int waitSeconds) {
+        int count = 0;
+        while (count < waitSeconds) {
+            if (this.isAuthenticated) {
+                return;
+            } else {
+                count++;
+                try {
+                    Thread.sleep(1000);
+                } catch (IllegalArgumentException e) {
+
+                } catch (InterruptedException e) {
+
+                }
+
+            }
+        }
+    }
+
+    private void processMessage(String message) {
+        System.out.println(message);
+        if (message.contains("Welcome, GLHF!")) {
+            this.isAuthenticated = true;
+            
+        } else if (message.startsWith("PING")) {
+            sendMessage(message.replace("PING", "PONG"));
+        }
+    }
+}

44949b00eb265c597a35e983c7a1dd04bf682d6a
Brian McClain <brianmmcclain@gmail.com>
2020-05-15 22:48:22 -0400

    Generated from start.spring.io

---
+++ src/test/java/com/github/brianmmcclain/reactivebotframework/ReactiveBotFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.brianmmcclain.reactivebotframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveBotFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

024d6adc8cb2e227e5ce44d904bab7d99e92055c
Brian McClain <brianmmcclain@gmail.com>
2020-09-03 11:52:10 -0400

    Remove unused constant

--- src/main/java/com/github/brianmmcclain/reactivebotframework/commands/Echo.java
+++ src/main/java/com/github/brianmmcclain/reactivebotframework/commands/Echo.java
@@ -3,9 +3,6 @@ package com.github.brianmmcclain.reactivebotframework.commands;
 import com.github.brianmmcclain.reactivebotframework.TwitchMessage;
 
 public class Echo extends BotCommand {
-    
-    private final String COMMAND = "echo";
-
     @Override
     public String execute(String data, TwitchMessage tMessage) {
         return "@" + tMessage.getSentBy() + " " + data;
